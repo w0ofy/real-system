d903bea8518f0c98691b7be94faeefb6f4c419dd
Mike Wolf <bigwoof91@gmail.com>
2022-08-28 14:12:37 -0400

    feat(all): remove emotion babel preset (#560)

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-const makePresets = (isDev) => [
+const presets = [
   [
     '@babel/preset-env',
     {
@@ -13,26 +13,6 @@ const makePresets = (isDev) => [
   ],
   '@babel/preset-react',
   '@babel/preset-typescript',
-  [
-    /** [@emotion/babel-preset-css-prop]
-     * This preset is used to automatically enable Emotion’s css prop when using the classic JSX runtime.
-     * This plugin is configurable with all options from the following plugins:
-     *  - `babel-plugin-transform-react-jsx`: https://babeljs.io/docs/en/babel-plugin-transform-react-jsx#options
-     * - `@emotion/babel-plugin`: https://github.com/emotion-js/emotion/tree/main/packages/babel-plugin
-     *
-     * React 17 introduced new JSX Transformations, which you can read about here: https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
-     * @NOTE If/when we adopt these new transformations, we should:
-     * 1. remove this preset
-     * 2. add the `@emotion/babel` plugin to our plugin section.
-     */
-    '@emotion/babel-preset-css-prop',
-    {
-      sourceMap: isDev,
-      autoLabel: 'dev-only',
-      labelFormat: '[local]',
-      cssPropOptimization: !isDev,
-    },
-  ],
 ];
 
 const makePlugins = (isDev) => [
@@ -63,17 +43,17 @@ module.exports = {
   env: {
     // used for bundling storybook
     production: {
-      presets: makePresets(true),
+      presets,
       plugins: makePlugins(true),
     },
     // used for compiling core
     release: {
-      presets: makePresets(false),
+      presets,
       plugins: makePlugins(false),
     },
     // used for storybook / development
     development: {
-      presets: makePresets(true),
+      presets,
       plugins: makePlugins(true),
     },
   },
