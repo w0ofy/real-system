acb978198eb190c5f4a057fb2e22da746b8ebccc
Mike Wolf <bigwoof91@gmail.com>
2022-09-03 01:04:45 -0400

    chore(infra): upgrading various deps (#565)

--- packages/libraries/utils-library/src/hooks/useSafeLayoutEffect.ts
+++ packages/libraries/utils-library/src/hooks/useSafeLayoutEffect.ts
@@ -6,7 +6,7 @@ const isBrowser = canUseDOM();
 /**
  * Safely use `useLayoutEffect` on server or client. If DOM is not accessible, `useEffect` (no-op) fn will be returned
  */
-const useSafeLayoutEffect = isBrowser ? useLayoutEffect : () => useEffect;
+const useSafeLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
 const useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
 
 export { useIsomorphicLayoutEffect, useSafeLayoutEffect };
