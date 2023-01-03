3c6d07e5204cde90755a88e1bd2ed94b1b17a882
Mike Wolf <mwolf@futurestay.com>
2023-01-02 19:40:49 -0500

    chore(storybook): clean storybook build script

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import dns from 'dns';
-
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-
-dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
