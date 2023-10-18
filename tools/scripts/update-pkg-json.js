dfd919a72cb9c53e6df3a4888fdc96cf4f90343f
Michael Wolf <bigwoof91@gmail.com>
2023-10-17 23:29:29 -0400

    fix(repo): fix tscheck hang

--- tools/scripts/update-pkg-json.js
+++ tools/scripts/update-pkg-json.js
@@ -1,4 +1,8 @@
-const { getWorkspacesInfo, writeToFile } = require('../utils');
+const {
+  getWorkspacesInfo,
+  logger,
+  // writeToFile
+} = require('../utils');
 const { readFileSync } = require('fs');
 
 (async function updatePkgJson() {
@@ -8,17 +12,22 @@ const { readFileSync } = require('fs');
     const pkgJson = readFileSync(pathToPkgJson, 'utf-8');
     const output = JSON.parse(pkgJson);
 
+    logger(output);
+
     /** add update code here
      * @example
      * ```js
+     * // previous use-cases
      * output.name = '@real-system/renamed-package'
+     * output.scripts['tscheck'] = 'tsc --noEmit --emitDeclarationOnly false';
      * ```
      */
 
-    writeToFile(pathToPkgJson, output, {
-      formatJson: true,
-      successMessage: `wrote ${output.name}.`,
-      errorMessage: `Failed to productionize ${pkgJson.name}.`,
-    });
+    /** uncomment below in order to overwrite workspace package.json files  */
+    // writeToFile(pathToPkgJson, output, {
+    //   formatJson: true,
+    //   successMessage: `wrote ${output.name}.`,
+    //   errorMessage: `Failed to productionize ${pkgJson.name}.`,
+    // });
   });
 })();
